f01b0f12d73ac1873ab59dd92121dd1050237293
Leo Silverberg <leosilverberg@gmail.com>
2017-08-24 12:37:10 +0100

    Clean up + fake server

---
+++ fakeServer.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, session, request
+from flask.ext.socketio import SocketIO, emit, join_room
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+import eventlet
+eventlet.monkey_patch()
+
+@app.route('/')
+def chat():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@socketio.on('message', namespace='/chat')
+def chat_message(message):
+    print(message['data']['message'])
+    print(classify(message['data']['message']))
+    emit('bot_message',{'data': response(message['data']['message'])}, broadcast = True)
+
+@socketio.on('connect', namespace='/chat')
+def test_connect():
+    emit('my response', {'data': 'Connected', 'count':0})
+
+if __name__ == '__main__':
+    CORS(app)
+    socketio.run(app,host='0.0.0.0', debug = False)
---
+++ templates/js/main.js
@@ -0,0 +1,23 @@
+var channel = "/chat";
+var socket = io.connect('http://' + document.domain + ':' + location.port + channel);
+var author = "leo";
+
+socket.on('connect', function() {
+    socket.on('bot_message', function(msg) {
+        console.log(msg.data);
+        $('#messages').append($('<li>').text(msg.data));
+    });
+});
+
+
+/**
+ * Sending message
+ */
+function sendMessage() {
+    console.log("sending");
+    socket.emit('message', { data: { message: $('#m').val(), author: author } });
+    $('#messages').append($('<li>').text($('#m').val()));
+    $('#m').val('');
+    $('#m').focus();
+    return false;
+};
