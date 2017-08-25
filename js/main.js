e8f6ed00b1f392e5f552400e66337ea0940fc24f
Leo Silverberg <leosilverberg@gmail.com>
2017-08-25 19:51:10 +0100

    try it

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var channel = "/chat";
+var socket = io.connect('http://' + document.domain + ':5000' + channel);
+var author = "leo";
+
+resizeMain()
+
+socket.on('connect', function() {
+    socket.on('bot_message', function(msg) {
+        console.log(msg.data);
+        $('#messages').append('<li class="bot-message"><div class="b-message">'+msg.data+'<br>'+msg.classification+'</div></li>');
+        updateScroll();
+    });
+});
+
+/**
+ * Scroll the messages div to bottom
+ */
+function updateScroll(){
+    
+    $(".main").animate({ scrollTop: $('.main').prop("scrollHeight")}, 1000);
+    console.log("scrolling");
+}
+
+/**
+ * Sending message
+ */
+function sendMessage() {
+    console.log("sending");
+    socket.emit('message', { data: { message: $('#m').val(), author: author } });
+    $('#messages').append('<li class="user-message"><div class="u-message">'+$('#m').val()+'</div></li>');
+    $('#m').val('');
+    $('#m').focus();
+    updateScroll();
+    return false;
+};
+
+
+$(window).resize(function() {
+    resizeMain()
+});
+
+
+function resizeMain(){
+    console.log("resize");
+    $('.main').height(
+        $(window).height() - ($('.header').height()+$('.text-input-form').height())
+    );
+}
